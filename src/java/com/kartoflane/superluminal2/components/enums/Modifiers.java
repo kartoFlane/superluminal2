668d11638e12ab2dc6563317899bb788ab048255
kartoFlane <tbachminski@gmail.com>
2014-08-16 22:08:39 +0200

    Fleshed out Modifiers enum a bit

--- src/java/com/kartoflane/superluminal2/components/enums/Modifiers.java
+++ src/java/com/kartoflane/superluminal2/components/enums/Modifiers.java
@@ -1,5 +1,52 @@
 package com.kartoflane.superluminal2.components.enums;
 
+/**
+ * Values conform with {@link org.eclipse.swt.SWT SWT}'s constants
+ * 
+ * @author kartoFlane
+ * 
+ * @see {@link org.eclipse.swt.SWT#ALT SWT.ALT}
+ * @see {@link org.eclipse.swt.SWT#SHIFT SWT.SHIFT}
+ * @see {@link org.eclipse.swt.SWT#CONTROL SWT.CONTROL}
+ * @see {@link org.eclipse.swt.SWT#COMMAND SWT.COMMAND}
+ *
+ */
 public enum Modifiers {
-	SHIFT, CONTROL, ALT, COMMAND;
+	// @formatter:off
+	ALT     (1 << 16),
+	SHIFT   (1 << 17),
+	CONTROL (1 << 18),
+	COMMAND (1 << 22);
+	// @formatter:on
+
+	private final int _id;
+
+	private Modifiers(int id) {
+		_id = id;
+	}
+
+	public int id() {
+		return _id;
+	}
+
+	public int getSWTMask() {
+		return _id;
+	}
+
+	@Override
+	public String toString() {
+		switch (this) {
+			case ALT:
+				return "Alt";
+			case SHIFT:
+				return "Shift";
+			case CONTROL:
+				return "Control";
+			case COMMAND:
+				return "Command";
+
+			default:
+				return "";
+		}
+	}
 }
