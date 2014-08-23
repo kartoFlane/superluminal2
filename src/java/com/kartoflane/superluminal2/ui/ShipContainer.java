475dfd76ce00f75d53f23abe6d8b506dc1f0bbad
kartoFlane <tbachminski@gmail.com>
2014-08-23 17:33:48 +0200

    Reverted a change that broke optimal offset feature

--- src/java/com/kartoflane/superluminal2/ui/ShipContainer.java
+++ src/java/com/kartoflane/superluminal2/ui/ShipContainer.java
@@ -539,8 +539,10 @@ public class ShipContainer implements Disposable, SLListener {
 		ShipObject ship = shipController.getGameObject();
 		for (Follower fol : shipController.getFollowers()) {
 			if (fol instanceof PropController == false && fol != hangarC) {
-				AbstractController ac = (AbstractController) fol;
-				ac.updateFollowOffset();
+				Point old = fol.getFollowOffset();
+				fol.setFollowOffset(old.x + (x - ship.getXOffset()) * CELL_SIZE,
+						old.y + (y - ship.getYOffset()) * CELL_SIZE);
+				fol.updateFollower();
 			}
 		}
 
